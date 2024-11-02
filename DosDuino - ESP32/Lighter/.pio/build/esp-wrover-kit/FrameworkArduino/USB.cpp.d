1bdb256febc4c307fca300f8a58ba17db6b53e6f
Niko Rastija <152702492+nrastija@users.noreply.github.com>
2024-11-02 16:35:50 +0100

    Delete DosDuino - ESP32/Lighter directory

--- DosDuino - ESP32/Lighter/.pio/build/esp-wrover-kit/FrameworkArduino/USB.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp-wrover-kit/FrameworkArduino/USB.cpp.o: \
- C:/Users/Niko/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
- C:/Users/Niko/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- C:/Users/Niko/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- DosDuino - ESP32/Lighter/.pio/build/esp-wrover-kit/FrameworkArduino/libb64/cdecode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp-wrover-kit/FrameworkArduino/libb64/cdecode.c.o: \
- C:/Users/Niko/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
- C:/Users/Niko/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
